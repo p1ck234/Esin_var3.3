acfa1fd4bf7127c4f04a5e034aa10e49a338b25f
Danil <93711640+p1ck234@users.noreply.github.com>
2022-12-09 17:52:31 +0400

    upl

---
+++ var3.3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Func(10);
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+
+        static int Func(int x)
+        {
+            if (x == 0)
+            {
+                return 2;
+            }
+            else if (x == 1)
+            {
+                return 3;
+            }
+            else if (x == 2)
+            {
+                return 4;
+            }
+            else
+            {
+                return Func(x - 2) + Func(x - 1) + Func(x - 3);
+            }
+        }
+    }
+}
